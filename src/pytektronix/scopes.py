dff0c41425340b4d9cabb460f60f21f180a6eb27
Matteo Vidali <mvidali@iu.edu>
2023-09-18 18:57:39 -0400

    update documentation - remove type specifiers

--- src/pytektronix/scopes.py
+++ src/pytektronix/scopes.py
@@ -18,9 +18,9 @@ class MDO3024:
     def __init__(self, resource_id: str=None, vxi11: bool = False, strict: bool = True):
         """
             Parameters:
-                - str resource_id: The VISA name or the VXI11 ip address of the scope. Leave blank if unknow, and a connection wizard will be run.
-                - bool vxi11: default False, set to true if connecting the the scope via VXI11 not VISA 
-                - bool strict: default True, set to False if errors should be soft and not result in a raised exception.
+                - resource_id: The VISA name or the VXI11 ip address of the scope. Leave blank if unknow, and a connection wizard will be run.
+                - vxi11: default False, set to true if connecting the the scope via VXI11 not VISA 
+                - strict: default True, set to False if errors should be soft and not result in a raised exception.
         """
 
         self.instr = LoggedVISA(resource_id=resource_id) if not vxi11 else LoggedVXI11(IP=resource_id)
@@ -95,7 +95,7 @@ class MDO3024:
             channel offset depending on probe resistance and the vertical scale. 
 
             Parameters:
-                - channel Channel: which channel to check the probe resistance of
+                Channel: which channel to check the probe resistance of
 
             Returns:
                 Tuple: (accepted values)
@@ -126,10 +126,10 @@ class MDO3024:
         """A scope method to set all trigger attributes desired.
 
             Parameters:
-                - str mode: desired trigger mode (normal, auto)
-                - str trig_type: desired trigger type (edge, logic, pulse, bus, video)
-                - str source: trigger source channel, any digital or logic channel allowed (ch<x> or d<x>)
-                - str level: desired level of the trigger in Volts, or (ttl, ecl)
+                - mode: desired trigger mode (normal, auto)
+                - trig_type: desired trigger type (edge, logic, pulse, bus, video)
+                - source: trigger source channel, any digital or logic channel allowed (ch<x> or d<x>)
+                - level: desired level of the trigger in Volts, or (ttl, ecl)
         """
 
         if mode:
@@ -145,6 +145,9 @@ class MDO3024:
     def get_trigger_info(self, setting: str=None) -> str:
         """A getter to retreive and show all settings currently set for the trigger.
            returns a string of all info.
+
+            Parameters:
+                setting: No Effect as of now... #TODO - get individual settings
         """
 
         return f"Mode: {self.trigger.mode}\n\
@@ -156,8 +159,8 @@ class MDO3024:
         """A scope method to set all horizontal attributes desired.
 
             Parameters:
-                - float scale: desired horizontal scale in Seconds, can be anything between (4e-10, 1000)
-                - float position: horizontal positioning expressed as a percentage of the screen (0, 100)
+                - scale: desired horizontal scale in Seconds, can be anything between (4e-10, 1000)
+                - position: horizontal positioning expressed as a percentage of the screen (0, 100)
         """
 
         if scale is not None:
@@ -168,6 +171,9 @@ class MDO3024:
     def get_horizontal_info(self, setting: str=None) -> str:
         """A getter to retreive and show all settings currently set for the Horizontal.
            returns a string of all info.
+        
+            Parameters:
+                setting: No Effect as of now... #TODO - get individual settings
         """
 
         return f"Scale: {self.horizontal.scale}\n \
@@ -178,11 +184,11 @@ class MDO3024:
         """A scope method to set all channel attributes desired.
 
             Parameters:
-                - str channel: channel for which settings are being changed
-                - float position: horizontal position of channel referenced to centerline: between (-8,8)
-                - float offset: vertical offset of channel referenced to centeriline (any)
-                - float scale: vertical scale of the desired channel
-                - str coupling: specify coupling of a given channel (AC, DC, DCREJECT)
+                - channel: channel for which settings are being changed
+                - position: horizontal position of channel referenced to centerline: between (-8,8)
+                - offset: vertical offset of channel referenced to centeriline (any)
+                - scale: vertical scale of the desired channel
+                - coupling: specify coupling of a given channel (AC, DC, DCREJECT)
         """
 
         if channel not in self.ch_dict.keys():
@@ -203,6 +209,9 @@ class MDO3024:
     def get_channel_info(self, channel: str) -> str:
         """A getter to retreive and show all settings currently set for the Channel.
            returns a string of all info.
+            
+            Parameters:
+                channel: a channel label (ch<x>, d<x>) of which to get info from
         """
 
         return f"{channel} Position: {self.ch_dict[channel].position}\n \
@@ -221,11 +230,11 @@ class MDO3024:
         """A scope method to set all waveform data related attributes.
 
             Parameters:
-                - str data_source: source of waveform data (ch<x>, d<x>, ref<x>, math, rf_*)
-                - str data_encoding: data output encoding (ascii, fastest, ribinary, rpbinary, sribinary srpbinary, fpbinary, sfpbinary)
-                - int data_width: width of output data in bytes (1,2,4,8 - encoding dependant)
-                - int data_start: at which sample to start the data capture (1 to 2e6)
-                - int data_stop: at which sample to stop the data capture (1 to 2e6)
+                - data_source: source of waveform data (ch<x>, d<x>, ref<x>, math, rf_*)
+                - data_encoding: data output encoding (ascii, fastest, ribinary, rpbinary, sribinary srpbinary, fpbinary, sfpbinary)
+                - data_width: width of output data in bytes (1,2,4,8 - encoding dependant)
+                - data_start: at which sample to start the data capture (1 to 2e6)
+                - data_stop: at which sample to stop the data capture (1 to 2e6)
         """
 
         if data_source is not None:
@@ -255,7 +264,7 @@ class MDO3024:
         """A scope method to caputure data from the scope returned as a bytearray, list, or np.ndarray.
 
             Parameters:
-                - str format: Specify return type as bytearray ('default'), list ('list') or numpy ndarray ('np_array')
+                - format: Specify return type as bytearray ('default'), list ('list') or numpy ndarray ('np_array')
         """
 
         data = self.waveform.get_data()
